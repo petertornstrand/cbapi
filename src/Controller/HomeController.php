21f91913ac905a282437459765b2f81223b48063
Peter Törnstrand <peter.tornstrand@happiness.se>
2025-10-09 08:45:11 +0200

    Integrate Twig for templating and create basic home page setup
    - Added `symfony/twig-bundle` dependency for templating.
    - Configured Twig in `twig.yaml`.
    - Created base template `base.html.twig` and home template `home.html.twig`.
    - Implemented `HomeController` to render the home page.
    - Updated `.env` for local development (`DEFAULT_URI` set to `https://localhost`).

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+/**
+ * Controller for home page.
+ */
+class HomeController extends AbstractController
+{
+    #[Route('/', methods: ['GET'])]
+    public function home(Request $request): Response {
+        $response = $this->render('home.html.twig');
+    }
+}
